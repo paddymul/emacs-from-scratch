4a778c1a19c9bcfa22f493865fcbc9e6a34def2b
Paddy Mullen <paddy@paddymullen.com>
2025-06-06 11:25:22 -0400

    basic vertico, marginalia, and savehist setup

---
+++ personal/paddy-completion-setup.el
@@ -0,0 +1,17 @@
+(use-package vertico
+  :ensure t
+  :config
+  (vertico-mode 1))
+
+(use-package marginalia
+  :ensure t
+  :config
+  (marginalia-mode 1))
+
+(use-package savehist
+  :ensure nil
+  :config
+  (savehist-mode 1)) 
+
+
+(provide 'paddy-completion-setup)
