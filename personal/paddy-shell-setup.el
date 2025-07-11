3a178816cf6d7235cd8e2cf1d73decf1429cdc88
Paddy Mullen <paddy@paddymullen.com>
2025-07-11 11:30:49 -0400

    utility for starting named compilation buffer

--- personal/paddy-shell-setup.el
+++ personal/paddy-shell-setup.el
@@ -1,2 +1,14 @@
 
+(defun paddy-compile-with-buffer-name
+    (local-compile-command buffer-name)
+    (compilation-start local-compile-command t
+		       (lambda (_mode) buffer-name) t))
+
+(defun paddy-shell-start-marimo ()
+  (interactive)
+  (paddy-compile-with-buffer-name
+   "/Users/paddy/buckaroo/.venv/bin/marimo edit"
+   "marimo-co-sh"))
+
+(paddy-shell-start-marimo)
 (provide 'paddy-shell-setup)
